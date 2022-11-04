703eaa1f7606a1e30639c4b625ae7be12a508c1a
Eva Pradhan <ejpradhan@yahoo.com>
2022-11-04 13:00:05 +0000

    adds fizzbuzz_spec.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,12 @@
 def fizzbuzz(number)
-  "fizz"
+  if number % 3 == 0
+    if number % 5 == 0
+      return "fizzbuzz"
+    end
+    return "fizz"
+  end
+  if number % 5 == 0
+    return "buzz"
+  end
+  number
 end
